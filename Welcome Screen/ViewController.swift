db4e279165e286e67823aa88ffd689ff5638bf68
burak <34burki24@gmail.com>
2017-12-25 14:52:55 +0300

    This app done

---
+++ Welcome Screen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Welcome Screen
+//
+//  Created by burak on 25.12.2017.
+//  Copyright © 2017 Burak Yıldırım. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
